7cb29963ae6da24b5a78b93592efdc007d0f0000
zhenerenya <zhenyachmchk@gmail.com>
2020-12-24 23:30:02 +0300

    add project

---
+++ cmake-build-debug/CMakeFiles/n.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/n.dir/main.cpp.o"
+  "n"
+  "n.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/n.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <random>
+#include <algorithm>
+#include "math.h"
+#include <map>
+#include <iomanip>
+
+double f (const double & x){
+    return exp((-0.2)*x)*sin(x)+1;
+}
+
+double _x (double a, double b){
+    return 1.*(b-a)*rand()/RAND_MAX+a;
+}
+
+double f_multi (const double & x){
+    return f(x)*sin(5*x);
+}
+
+
+int main() {
+    double A, B; //диапозон поиска
+    A=9;
+    B=12;
+    srand(time(NULL));
+    std::vector<double>P;
+    std::vector<double>q;
+    for (double i=0.9; i<1; i+=0.01){
+        P.push_back(i);
+    }
+    for (double i=0.005; i<=0.1; i+=0.005){
+        q.push_back(i);
+    }
+
+
+    for (const auto& i : P){
+        std::cout<<"|"<<std::setw(4)<<"N"<<"|"<<std::setw(8)<<"P"<<"|"<<std::setw(10)<<"q"<<"|"<<
+                 std::setw(8)<<"x_min"<<"|"<<std::setw(8)<<"f_min"<<"|"<<std::endl;
+        double x;
+        for (const auto& j : q){
+            int N = ceil(log(1-i)/log(1-j));
+            double f_min=f(A);
+            double x_min=A;
+            for (int k=0; k<N; k++){
+                x=_x(A, B);
+                if (f_min>=f(x)) {
+                    f_min=f(x);
+                    x_min=x;
+                }
+            }
+            std::cout<<"|"<<std::setw(4)<<N<<"|"<<std::setw(8)<<i<<"|"<<std::setw(8)<<j<<"|"<<
+                     std::setw(8)<<x_min<<"|"<<std::setw(8)<<f_min<<"|"<<std::endl;
+
+        }
+
+        std::cout<<std::setfill('=')<<std::setw(42)<<" ";
+        std::cout<<std::endl<<std::setfill(' ');
+
+    }
+
+    std::cout<<"FOR MULTI F(X)*SIN(5X)"<<std::endl;
+
+    for (const auto& i : P){
+        std::cout<<"|"<<std::setw(5)<<"N"<<"|"<<std::setw(10)<<"P"<<"|"<<std::setw(10)<<"q"<<"|"<<
+                 std::setw(10)<<"x_min"<<"|"<<std::setw(10)<<"f_min"<<"|"<<std::endl;
+        double x;
+        for (const auto& j : q){
+            int N = ceil(log(1-i)/log(1-j));
+            double f_min=f_multi(A);
+            double x_min=A;
+            for (int k=0; k<N; k++){
+                x=_x(A, B);
+                if (f_min>=f_multi(x)) {
+                    f_min=f_multi(x);
+                    x_min=x;
+                }
+            }
+            std::cout<<"|"<<std::setw(5)<<N<<"|"<<std::setw(10)<<i<<"|"<<std::setw(10)<<j<<"|"<<
+                     std::setw(10)<<x_min<<"|"<<std::setw(10)<<f_min<<"|"<<std::endl;
+
+        }
+
+        std::cout<<std::setfill('=')<<std::setw(52)<<" ";
+        std::cout<<std::endl<<std::setfill(' ');
+
+    }
+
+
+    return 0;
+}
